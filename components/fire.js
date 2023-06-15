a2612c060ac89198eb24c0ff23c0946a5d342ecb
Fumiya-Kai <bunjyo.science@icloud.com>
2023-06-15 22:58:59 +0900

    firebaseの設定ファイル作成

---
+++ components/fire.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiIYDvZ8tj2uccsRga-hW18bqdEgO2ZEA",
+  authDomain: "fumiya-react-practice.firebaseapp.com",
+  projectId: "fumiya-react-practice",
+  storageBucket: "fumiya-react-practice.appspot.com",
+  messagingSenderId: "848155052085",
+  appId: "1:848155052085:web:aa12601343c6e58003c97c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
